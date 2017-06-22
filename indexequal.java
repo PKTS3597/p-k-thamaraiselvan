fe894a84b9a78d6f8b061b04dd8fda49063295e2
thamaraiselvan p k <thamaraiselvan3535@gmail.com>
2017-06-22 15:21:17 +0530

    Add files via upload

---
+++ indexequal.java
@@ -0,0 +1,29 @@
+package guviii;
+import java.util.*;
+public class indexequal {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the array size");
+		int size=s.nextInt();
+		System.out.println("enter the array values");
+		int arr[]=new int[size];
+		int status=0;
+for(int i=0;i<size;i++)
+{
+	arr[i]=s.nextInt();
+}
+for(int i=0;i<size;i++)
+{
+	if(arr[i]==i)
+	{
+		status++;
+		System.out.println("the number "+arr[i]+" is equal to "+i);
+	}
+}
+if(status==0)
+{
+	System.out.println("There is no number equal to index");
+}
+
+	}
+}
